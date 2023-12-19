bab2b2c6a30bb74637e3f39b5e5ed7cc59c275d8
tepes <wadoryuromania@gmail.com>
2023-12-19 21:16:01 +0200

    End of Section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -7,30 +7,36 @@ import { Type } from '../shared/models/type';
 import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
-   baseUrl ='https://localhost:5001/api/';
-  constructor(private http:HttpClient) { }
+  baseUrl = 'https://localhost:5001/api/';
+  constructor(private http: HttpClient) {}
 
-  getProducts(shopParams:ShopParams){
-
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
-    if(shopParams.brandId > 0) params= params.append('brandId',shopParams.brandId);
-    if(shopParams.typeId) params = params.append('typeId',shopParams.typeId);
+    if (shopParams.brandId > 0)
+      params = params.append('brandId', shopParams.brandId);
+    if (shopParams.typeId) params = params.append('typeId', shopParams.typeId);
     //if(sort) params = params.append('sort',sort);
-     params = params.append('sort',shopParams.sort);
-     params = params.append('pageIndex',shopParams.pageNumber);
-     params = params.append('pageSize',shopParams.pageSize);
-     if(shopParams.search)      params = params.append('search',shopParams.search);
-    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products',{params});
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
+    if (shopParams.search) params = params.append('search', shopParams.search);
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {
+      params,
+    });
   }
 
-  getBrands(){
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
+  getBrands() {
     return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
   }
 
-  getTypes(){
+  getTypes() {
     return this.http.get<Type[]>(this.baseUrl + 'products/types');
   }
 }
